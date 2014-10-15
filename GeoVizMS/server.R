8fa74e13de96ba55da57a8a1a7f1e657e83f8718
LiNk-NY <mramos09@gmail.com>
2014-10-15 01:17:53 -0400

    second attempt

--- GeoVizMS/server.R
+++ GeoVizMS/server.R
@@ -3,36 +3,34 @@ source("preproc.r")
 shinyServer(
         function(input, output) {
 
-        dataSubset <- reactive({
-                subset(metsyn, YEAR %in% input$year) 
-                })
-
+        met <- reactive({ 
+          subset(metsyn, YEAR==input$year)
+        })
         in_sex <- reactive({input$sex})
         in_agegroup <- reactive({input$agegroup})
         in_race <- reactive({input$race})
         
         tab_w <- reactive({
-                in_tab <- dataSubset()[dataSubset()$SEX %in% as.character(in_sex()),]
+                in_tab <- met()[met()$SEX %in% as.character(in_sex()),]
                 in_tab <- in_tab[in_tab$AGEG %in% as.character(in_agegroup()),]
                 in_tab <- in_tab[in_tab$IMPRACE %in% as.character(in_race()),]
                 
-                tab <- round(with(in_tab, xtabs(STDPROP~STATE+METSD))*100,3)
+                tab <- round(with(in_tab, xtabs(STDFQ~METSD+STATE))*100,3)
         
                 tab_l <- melt(tab)
                 tab_w <- dcast(tab_l, STATE~METSD)
                 tab_w$sum <- tab_w$No + tab_w$Yes
                 tab_w$prop <- tab_w$Yes/ tab_w$sum
                 tab_w$percent <- tab_w$prop*100
-        
+                tab_w$SNAME <- fips$STATE[match(tab_w$STATE, fips$FIPS)]
+                tab_w$REGION <- fips$region[match(tab_w$STATE, fips$FIPS)]
                         })
                 
 
         output$geomap <- renderGvis({
-        
-                gvisGeoChart(as.data.frame(tab_w()), colorvar="STDPROP", locationvar="REGION", hovervar="SNAME",
-                                options=list(region="US", dataMode="regions", resolution="provinces", 
-                                title="Metabolic Syndrome Prevalence Proportions", 
-                                titlePosition='out') 
+                gvisGeoChart(data=as.data.frame(tab_w()), colorvar="percent", locationvar="REGION", hovervar="SNAME",
+                                options=list(region="US", dataMode="region", resolution="provinces", 
+                                title="Metabolic Syndrome Prevalence Proportions") 
                                 )
                 })
 
--- GeoVizMS/ui.R
+++ GeoVizMS/ui.R
@@ -8,28 +8,28 @@ shinyUI(fluidPage(
      sidebarPanel(
         radioButtons("year", label= h3("Choose a BRFSS Year:"), 
                          choices= list("BRFSS 2011" = 2011, "BRFSS 2013" = 2013), 
-                         selected=2011), 
+                         selected=2011, inline=TRUE), 
         
       helpText("Visualize metabolic syndrome by:"),
                 
       checkboxGroupInput("sex", 
-                  label = "Choose which sex to display",
+                  label = "Choose Gender",
                   choices = c("Male", "Female"),
                   selected = c("Male", "Female")),
       
       checkboxGroupInput("agegroup",
-                  label = "Choose which age categories to display",
+                  label = "Choose Age Group(s)",
                   choices = c("18-24", "25-34", "35-44", "45-54", "55-64", ">65"),
                   selected = c("18-24", "25-34", "35-44", "45-54", "55-64", ">65")),
       
       checkboxGroupInput("race",
-                  label = "Choose which races to display",
-                  choices = c("NH White", "NH Black", "NH Asian", "NH NA/AN", "Hispanic", "Other"),
+                  label = "Choose Racial/Ethnic Group(s)",
+                  choices = c("NH White", "NH Black", "NH Asian", "NH Native American/Alaskan Native"="NH NA/AN", "Hispanic", "Other"),
                   selected = c("NH White", "NH Black", "NH Asian", "NH NA/AN", "Hispanic", "Other")), 
-      submitButton(text="Submit")),
+      submitButton(text="Submit", icon("share"))        ),
      
     mainPanel(
-             htmlOutput("geomap")
+             htmlOutput(outputId="geomap")
              )
               )
 ))
