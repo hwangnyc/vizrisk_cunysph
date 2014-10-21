ae4288cd96dac5f630a775ed7df6add4b7a35da6
Levi Waldron <lwaldron.research@gmail.com>
2014-10-20 23:31:30 -0400

    allow input x to be NULL

--- colpalette.R
+++ colpalette.R
@@ -12,12 +12,16 @@ getCols <- function(x=NULL,  #Numeric vector to calculated colors for
                     text.offset=1){ #amount by which to offset text
   n.categories <- length(fixed.cutpoints)
   col.palette = colorRampPalette(c(zero.col, saturated.col))(n.categories)
-  x[x < min(fixed.cutpoints)] <- NA
-  x[is.na(x)] <- -Inf
-  mycols <- as.character(cut(x, c(fixed.cutpoints, Inf), labels=col.palette, include.lowest=TRUE))
-  mycols[is.na(mycols)] <- missing.col
+  if(!is.null(x)){
+      x[x < min(fixed.cutpoints)] <- NA
+      x[is.na(x)] <- -Inf
+      mycols <- as.character(cut(x, c(fixed.cutpoints, Inf), labels=col.palette, include.lowest=TRUE))
+      mycols[is.na(mycols)] <- missing.col
+  }else{
+      mycols <- NULL
+  }
   if(plotlegend){
-    labs <- c(paste(fixed.cutpoints[-length(fixed.cutpoints)], fixed.cutpoints[-1], sep=" - "), 
+    labs <- c(paste(fixed.cutpoints[-length(fixed.cutpoints)], fixed.cutpoints[-1], sep=" - "),
               paste(">", fixed.cutpoints[length(fixed.cutpoints)], sep=""))
     labs <- paste(rev(labs), append.labs, sep="")
     if(missing.col %in% mycols){
@@ -25,9 +29,9 @@ getCols <- function(x=NULL,  #Numeric vector to calculated colors for
       col.palette <- c(missing.col, col.palette)
     }
     plot(0:10, 0:10, type="n", xaxt="n", yaxt="n", bty="n", xlab="", ylab="")
-    points(x=rep(1, length(col.palette)), y=length(col.palette):1, 
+    points(x=rep(1, length(col.palette)), y=length(col.palette):1,
            pch=15, cex=box.cex, col=rev(col.palette))
-    text(labs, x=rep(1+text.offset, length(col.palette)), 
+    text(labs, x=rep(1+text.offset, length(col.palette)),
          y=length(col.palette):1, pos=4, cex=text.cex)
   }
   return(mycols)
@@ -38,6 +42,6 @@ y <- 0:100
 y[10:15] <- NA
 y[70:75] <- -99
 
-##Create a legend and 
+##Create a legend and
 mycols <- getCols(y, fixed.cutpoints=c(0, 20, 50, 75))
 plot(0:100, col=mycols)
