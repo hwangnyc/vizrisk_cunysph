3b52d14536bbb3f76126b57f6435222190264ee1
LiNk-NY <mramos09@gmail.com>
2014-10-27 01:47:57 -0400

    working on tab panels

---
+++ GeoVizMS/about.r
@@ -0,0 +1,13 @@
+function(){
+        tabPanel("About",
+                 HTML('<div style="float: right; margin: 5px 5px 5px 10px;"> 
+                     <iframe width="560" height="315" src="//www.youtube.com/embed/uHYcDQDbMY8" frameborder="0" allowfullscreen></iframe> 
+                      </div>'),
+                 h4("About Us"),
+                 p("Insert Text Here"),
+                 hr(),
+                 h4("Methods"),
+                 p("Insert Methods Here"),
+                 value="about"
+                 )
+}
--- GeoVizMS/ui.R
+++ GeoVizMS/ui.R
@@ -7,6 +7,8 @@ library(gridExtra)
 suppressPackageStartupMessages(library(googleVis))
 source("preproc.r")
 
+tabPanelAbout <- source("about.r")$value
+
 shinyUI(
  fluidPage(
     fluidRow(column(8, titlePanel("Metabolic Syndrome: Where Does Your State Stand?")),
@@ -37,35 +39,37 @@ fluidRow(
                   label = "Racial/Ethnic Group",
                   choices = c("NH White", "NH Black", "NH Asian", "NH Native American/Alaskan Native"="NH NA/AN", "Hispanic", "Other"),
                   selected = c("NH White", "NH Black", "NH Asian", "NH NA/AN", "Hispanic", "Other")) 
-                )),
-     
-      column(6,
-        h4("Metabolic Syndrome Prevalence Proportion by State"),
-        htmlOutput(outputId="geotab"),
-        helpText("Data are presented in percentages, age-adjusted to the nationwide age distribution from the 2010 Census. 
-                 Darker colors indicate a higher prevalence of metabolic syndrome."),
-        p("Metabolic Syndrome was defined as having 3 or more of the following risk factors: a Body Mass Index (BMI) greater than 25, 
-        having been told by a primary care provider about the presence of high cholesterol, diabetes, or high blood pressure/hypertension."),
-        hr(),
-        h4(HTML(paste("Contributing Factors: Availability of Health Food and Fast Food",tags$sup("3,4"), sep=" "))),
-        p("Metabolic syndrome may be associated with certain dietary and lifestyle choices. 
-        The bubbleplots below illustrate the relationship between the prevalence of metabolic syndrome and availability
-        of farmers' markets and fast food by state and region."),
-        br(),
-             htmlOutput(outputId="farmers"),
-             htmlOutput(outputId="fastfood"),
-             helpText("*Data available only for 43 states.")
-        ),
-      column(1, br(),img(src="legend.png", height=362, width=121,align="left", style="margin-left:10px")),
-    
-      column(3, br(),
+                        ),
+                ),
+        column(6,
+                tabsetPanel(
+                        tabPanel(title="Metabolic Syndrome Map", 
+                                h4("Metabolic Syndrome Prevalence Proportion by State"),
+                                htmlOutput(outputId="geotab"),
+                                helpText("Data are presented in percentages, age-adjusted to the nationwide age distribution from the 2010 Census. 
+                                 Darker colors indicate a higher prevalence of metabolic syndrome."),
+                                p("Metabolic Syndrome was defined as having 3 or more of the following risk factors: a Body Mass Index (BMI) greater than 25, 
+                                having been told by a primary care provider about the presence of high cholesterol, diabetes, or high blood pressure/hypertension."),
+                                hr(),
+                                h4(HTML(paste("Contributing Factors: Availability of Health Food and Fast Food",tags$sup("3,4"), sep=" "))),
+                                p("Metabolic syndrome may be associated with certain dietary and lifestyle choices. 
+                                The bubbleplots below illustrate the relationship between the prevalence of metabolic syndrome and availability
+                                of farmers' markets and fast food by state and region."),
+                                br(),
+                                htmlOutput(outputId="farmers"),
+                                htmlOutput(outputId="fastfood"),
+                                helpText("*Data available only for 43 states."),
+                                column(1, br(),img(src="legend.png", height=362, width=121,align="left", style="margin-left:10px"))), 
+                tabPanelAbout()
+                        )
+               ),
+     column(3, br(),
            wellPanel(
                    selectInput("state", label=h3("State Viewer"), 
                          choices= unique(metsyn$SNAME)),
                    plotOutput(outputId="stateview")
-                )
-             )
-    )
-  ) 
+                    )
+            )
+     )
+   )
 )
-
