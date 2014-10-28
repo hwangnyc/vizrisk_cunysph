e20be0576cd3e42b93029d01963c5e826ba204e7
LiNk-NY <mramos09@gmail.com>
2014-10-28 13:46:11 -0400

    server changes

--- GeoVizMS/server.R
+++ GeoVizMS/server.R
@@ -37,11 +37,11 @@ shinyServer(
     output$farmers <- renderGvis({
       
       cast1 <- merge(tab_w(), fmf, by.x = "STATE", by.y = "fips")
-      colnames(cast1)[grep("capfarmkt", colnames(cast1))] <- "MarketsPerCap"
-      cast1$MarketsPerCap <- round(cast1$MarketsPerCap, 2)
+      colnames(cast1)[grep("capfarmkt", colnames(cast1))] <- "MarketsPerPop"
+      cast1$MarketsPerPop <- round(cast1$MarketsPerPop, 2)
       cast1 <- cbind(cast1, Population.Proportion=round((cast1$popfm/sum(cast1$popfm))*100,2))
       
-      return(gvisBubbleChart(cast1, idvar="ABB", xvar="MS.Proportion", yvar="MarketsPerCap", 
+      return(gvisBubbleChart(cast1, idvar="ABB", xvar="MS.Proportion", yvar="MarketsPerPop", 
                              sizevar="Population.Proportion",colorvar="SubRegion",
                              options=list(width=800,
                                           height=400,
@@ -57,11 +57,11 @@ shinyServer(
     output$fastfood <- renderGvis({
       
       cast1 <- merge(tab_w(), fmf, by.x = "STATE", by.y = "fips")
-      colnames(cast1)[grep("capfstfd", colnames(cast1))] <- "FastFoodPerCap"
-      cast1$FastFoodPerCap <- round(cast1$FastFoodPerCap, 2)
+      colnames(cast1)[grep("capfstfd", colnames(cast1))] <- "FastFoodPerPop"
+      cast1$FastFoodPerPop <- round(cast1$FastFoodPerPop, 2)
       cast1 <- cbind(cast1, Population.Proportion=round((cast1$popff/sum(cast1$popff))*100,2))
       
-      return(gvisBubbleChart(cast1, idvar="ABB", xvar="MS.Proportion", yvar="FastFoodPerCap", 
+      return(gvisBubbleChart(cast1, idvar="ABB", xvar="MS.Proportion", yvar="FastFoodPerPop", 
                              sizevar="Population.Proportion",colorvar="SubRegion",
                              options=list(width=800,
                                           height=400,
